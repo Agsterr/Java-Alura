20c21625c58678852351c97680ae519928fbdc1e
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-04-13 15:39:46 -0300

    Atualização screenmatch

--- src/br/com/alura/Principal/PrincipalComListas.java
+++ src/br/com/alura/Principal/PrincipalComListas.java
@@ -11,18 +11,35 @@ public class PrincipalComListas {
 
 
         var filmeDoPaulo = new Filme("dogville", 2003);
-
+        filmeDoPaulo.avalia(10);
         Filme meuFilme = new Filme("o poderoso chefão ", 1970);
+        meuFilme.avalia(9);
 
-
-        Filme filme = new Filme("o chefão", 1970);
-
+        Filme filmee = new Filme("o chefão", 1970);
+        filmee.avalia(6);
         Series lost = new Series("lost",2000);
         ArrayList <Titulos> listaDeFilmes = new ArrayList<>();
         listaDeFilmes.add(filmeDoPaulo);
+
         listaDeFilmes.add(meuFilme);
-        listaDeFilmes.add(filme);
+
+        listaDeFilmes.add(filmee);
         listaDeFilmes.add(lost);
+        for (Titulos listaDeFilme : listaDeFilmes) {
+
+            System.out.println(listaDeFilme.getNome());
+           //casting
+           // Filme filme = (Filme) listaDeFilme;
+           // System.out.println("classificação: " + filme.getClassificacao() );
+           if (listaDeFilme instanceof Filme filme && filme.getClassificacao() > 2){
+               System.out.println("classificação: " + filme.getClassificacao() );
+           }
+
+//            System.out.println("----------------------------------------------------");
+//            listaDeFilmes.forEach(nome -> System.out.println(nome));
+//            System.out.println("----------------------------------------------------");
+//            listaDeFilmes.forEach(System.out::println);
+        }
 
 
     }
