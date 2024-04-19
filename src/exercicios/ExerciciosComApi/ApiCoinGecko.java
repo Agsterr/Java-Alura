8dd406c36e5a907eb59300812e67b27bcbebefb5
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-04-19 16:24:54 -0300

    finalização exercicios de api

--- src/exercicios/ExerciciosComApi/ApiCoinGecko.java
+++ src/exercicios/ExerciciosComApi/ApiCoinGecko.java
@@ -7,20 +7,21 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class ApiGoogleBooks {
+public class ApiCoinGecko {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
 
-        System.out.println("Digite o título do livro para a busca: ");
-        var tituloLivro = leitura.nextLine();
+        System.out.println("Digite o título da moeda para a busca: ");
+        var moeda = leitura.nextLine();
         //substitui o espaço por %20
-        String nomeDoLivroComEspaco = tituloLivro.replace(" ","%20");
-        String chave = "AIzaSyBwjMMzKDMnB1Lfb64vlM9tG5h9MOvWz7I";
-        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + nomeDoLivroComEspaco + "&key=" + chave;
+        String nomeDaMoeda = moeda.replace(" ","%20");
+
+        String endereco = "https://api.coingecko.com/api/v3/simple/price?ids="+nomeDaMoeda +"&vs_currencies=usd";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+
 
     }
 }
